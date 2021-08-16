ed9214284be4c34d2b63f85dd3fd0e707fb474ef
Peter Racine Jr <peterracinejr@gmail.com>
2021-08-16 12:39:14 -0500

    styled the update/save foodItem button

--- src/components/foodItem/FoodItemCard.js
+++ src/components/foodItem/FoodItemCard.js
@@ -132,32 +132,3 @@ export const FoodItemCard = ({ foodItem }) => {
     </>
   );
 };
-
-// {
-//   /* <section className="foodItem">
-// <div>
-//   <div className="food_item_photo">{foodItem.photo}</div>
-// </div>
-// <div className="food_item_name">{foodItem.name}</div>
-// <div className="food_item_homeId">{foodItem.home?.name}</div>
-// <div className="food_item_homeId">{foodItem.category?.name}</div>
-// <div className="food_item_storageAreaId">
-//   {foodItem.storageArea?.name}
-// </div>
-// <div className="food_item_price">${foodItem.price}</div>
-// <div className="food_item_datePurchased">
-//   Date Purchased: {foodItem.datePurchased}
-// </div>
-// <div className="food_item_expirationDate">
-//   Expiration Date: {foodItem.expirationDate}
-// </div>
-// {expiredFood ? (
-//   <div className="expiration_alert">THIS ITEM IS EXPIRED</div>
-// ) : (
-//   <></>
-// )}
-// <button className="button" onClick={handleEdit}>
-//   Edit
-// </button>
-// </section> */
-// }
